462ece7c6c2e2170245e410450f80fecd34f6bf7
BillyTziv <vtzivaras@gmail.com>
2015-01-05 19:35:04 +0200

    Newer version

--- JarvisAutoHover.ino
+++ JarvisAutoHover.ino
@@ -37,9 +37,12 @@ int minimumRange = 0;	// Minimum range needed
 
 long duration1, duration2, duration3, duration4;
 long distance1, distance2, distance3, distance4;
+int stabilizationPoint = 0;
 
 #define MIN_MV 60		// Minimum motor value after the ARM mode
 #define MAX_MV 100		// Maximum motor value after the ARM mode
+
+#define distanceErrorNumber 5;
 
 Servo M1;				// Front left motor
 Servo M2;				// Front right motor
@@ -51,65 +54,46 @@ int RFMV = MIN_MV;		// Value fo the right Front motor [M2]
 int RBMV = MIN_MV;		// Value of the right back motor [M3]
 int LBMV = MIN_MV;		// Value of the left back motor [M4]
 
+int distArray1[5];
+int distArray2[5];
+int distArray3[5];
+int distArray4[5];
+
 /*
 * Inialize the motor pins and write the min value (arm the motors)
 */
 void initMotors(void) {
-     	M1.attach(10);
-     	M2.attach(11);
-     	M3.attach(12);
-     	M4.attach(13);
+	M1.attach(10);
+	M2.attach(11);
+	M3.attach(12);
+	M4.attach(13);
 }
 
 void arm(void) {
   	Serial.println("I am back to life ^_^");
   	M1.write(20);
-     	M2.write(20);
-     	M3.write(20);
-     	M4.write(20);
-       
-     	delay(10000);    // 10 seconds
-       
-     	Serial.println("Motors have been armed...\n");
+	M2.write(20);
+	M3.write(20);
+	M4.write(20);
+  
+	delay(10000);    // 10 seconds
+  
+	Serial.println("Motors have been armed...\n");
 }
 
 void printHeaders() {
-  Serial.print("HC-1");
-
-	Serial.print("\t");
-
-	Serial.print("HC-2");
-
-	Serial.print("\t");
-
-	Serial.print("HC-3");
-
-	Serial.print("\t");
-
-	Serial.print("HC-4");
-
-	Serial.print("\t");
-
+	Serial.print("HC-1\t");
+	Serial.print("HC-2\t");
+	Serial.print("HC-3\t");
+	Serial.print("HC-4\t");
 	
-
-	Serial.print("M1");
-
-	Serial.print("\t");
-
-	Serial.print("M2");
-
-	Serial.print("\t");
-
-	Serial.print("M4");
-
-	Serial.print("\t");
-
-	Serial.print("M3");
-
-	Serial.print("\t");
-
+	Serial.print("M1\t");
+	Serial.print("M2\t");
+	Serial.print("M4\t");
+	Serial.print("M3\t");
+	Serial.print("SP\t");
 	
-
+	
 	Serial.println();  
 }
 
@@ -151,39 +135,24 @@ int calculateDistance(int trig, int echo) {
 }
 
 void printStatus() {
-	/* Send the distance to the computer using Serial protocol, and
-
-	turn LED OFF to indicate successful reading. */
-
-	
-
 	Serial.print(distance1);
-
 	Serial.print("\t");
-
 	Serial.print(distance2);
-
 	Serial.print("\t");
-
 	Serial.print(distance3);
-
 	Serial.print("\t");
-
 	Serial.print(distance4);
-
+	Serial.print("\t");
 	Serial.print(LFMV);
-
 	Serial.print("\t");
-
 	Serial.print(RFMV);
-
 	Serial.print("\t");
-
 	Serial.print(LBMV);
-
 	Serial.print("\t");
-
 	Serial.print(RBMV);
+	Serial.print("\t");
+	Serial.print(stabilizationPoint);
+	
 
 	Serial.println();
 }
@@ -238,22 +207,6 @@ void autoLanding() {
 	*/
 }
 
-
-/*
- * Operation 1
- * Procees with steady motor value at four motors
- *
- * Status: Completed
-*/
-void steadyMotor(void) {
-	M1.write(MIN_MV);
-	M2.write(MIN_MV);
-	M3.write(MIN_MV);
-	M4.write(MIN_MV);
-}
-
-
-
 /*
  * Finds the center point (average value) and stabilizes the quadcopter
  * writting more or less throttle to motors. The sensors are 4 points into
@@ -263,58 +216,59 @@ void steadyMotor(void) {
  * Control the motors giving more or less throttle to stabilize the quad
 */
 void stabilize(int point1, int point2, int point3, int point4) {
-	int stabilizationPoint = 0;
+	int step = 1;
+	int delayTime = 20;
+	
 		stabilizationPoint = (point1+point2+point3+point4)/4;
 		if( stabilizationPoint >= distance1) {
 			// Increase the throttle
-			LFMV = LFMV+3;
-			delay(50);
+			LFMV = LFMV+step;
+			delay(delayTime);
 		}else {
-			LFMV = LFMV-3;
-			delay(50);
+			LFMV = LFMV-step;
+			delay(delayTime);
 		}
 		
 		if( stabilizationPoint >= distance2) {
 			// Increase the throttle
-			RFMV = RFMV+3;
-			delay(50);
+			RFMV = RFMV+step;
+			delay(delayTime);
 		}else {
-			RFMV = RFMV-3;
-			delay(50);
+			RFMV = RFMV-step;
+			delay(delayTime);
 		}
 		
 		if( stabilizationPoint >= distance3) {
 			// Increase the throttle
-			LBMV = LBMV+3;
-			delay(50);
+			LBMV = LBMV+step;
+			delay(delayTime);
 		}else {
-			LBMV = LBMV-3;
-			delay(50);
+			LBMV = LBMV-step;
+			delay(delayTime);
 		}
 		
 		if( stabilizationPoint >= distance4) {
 			// Increase the throttle
-			RBMV = RBMV+3;
+			RBMV = RBMV+step;
 			delay(50);
 		}else {
-			RBMV = RBMV-3;
+			RBMV = RBMV-step;
 			delay(50);
 		}
 }
 
 void engine() {
-    if((LFMV >= 70) || (RFMV >= 70) || (RBMV >= 70) || (LBMV >= 70)) {
-      M1.write(MIN_MV);
-      M2.write(MIN_MV);
-      M3.write(MIN_MV);
-      M4.write(MIN_MV);
-    }else {
+    if(stabilizationPoint < 30) {
       M1.write(LFMV+1);
       M2.write(RFMV+1);
       M3.write(RBMV+1);
       M4.write(LBMV+1);
+     }else {
+      M1.write(LFMV-1);
+      M2.write(RFMV-1);
+      M3.write(RBMV-1);
+      M4.write(LBMV-1);
     }
-  delay(1000);
 }
 
 /*
@@ -322,16 +276,16 @@ void engine() {
  * Debugging purpose!
 */
 void virtualEngine() {
-	if((LFMV >= 70) || (RFMV >= 70) || (RBMV >= 70) || (LBMV >= 70)) {
-      LFMV = MIN_MV;
-      RFMV = MIN_MV;
-      RBMV = MIN_MV;
-      LBMV = MIN_MV;
-    }else {
+    if(stabilizationPoint < 30) {
       LFMV = LFMV+1;
       RFMV = RFMV+1;
       RBMV = RBMV+1;
       LBMV = LBMV+1;
+    }else {
+      LFMV = LFMV-1;
+      RFMV = RFMV-1;
+      RBMV = RBMV-1;
+      LBMV = LBMV-1;
     }
   delay(20);
 }
@@ -343,11 +297,10 @@ void loop() {
 	distance4 = calculateDistance(trigPin4, echoPin4);
 
 	//engine();
-        virtualEngine();
-	stabilize(distance1, distance2, distance3, distance4);
+     virtualEngine();
+	//stabilize(distance1, distance2, distance3, distance4);
 	
 	printStatus();
-	//...
      //guard();
 	//Delay 50ms before next reading.
 	delay(50);
