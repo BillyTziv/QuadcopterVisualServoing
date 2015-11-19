d5b85a52c680c9ec9f349a93d589e86972f10e51
Vasilis Tzivaras <vtzivaras@gmail.com>
2015-11-19 11:43:00 +0200

    updates

--- MATLAB/v1.10/simulator.m
+++ MATLAB/v1.10/simulator.m
@@ -17,21 +17,22 @@ d = 0.25;                       % length of the rods (cm)
 ct = 3e-6;                      % thrust coefficient N
 cq = 1e-7;                      % torque due to drag coefficient N
 
-kfx = 0.1;                        % Desired force position constant
-kfv = 100;                        % Desired force velocity constant
+kp = 1;                        % PID controller proportional gain
+ki = 1;                        % PID controller integral gain
+kd = 1;                        % PID controller derivative gain
 
-kvx = 0.6;                      % Desired velocity constant in X axis
-kvy = 0.6;                      % Desired velocity constant in X axis
+kvx = 1;                      % Desired velocity constant in X axis
+kvy = 1;                      % Desired velocity constant in X axis
 kthetax = 1;                    % Desired angle constant in X axis
 kthetay = 1;                    % Desired angle constant in X axis
 
-kttx = 0.065;                    % Torque theta constant on X axis
-ktty = 0.065;                    % Torque theta constant on Y axis
-kttz = 0.08;                     % Torque theta constant on Z axis
+kttx = 1;                    % Torque theta constant on X axis
+ktty = 1;                    % Torque theta constant on Y axis
+kttz = 1;                     % Torque theta constant on Z axis
 
-ktx = 0;                      % X axis thetadot factor
-kty = 0;                      % Y axis thetadot factor
-ktz = 0.018;                      % Z axis thetadot factor
+ktx = 1;                      % X axis thetadot factor
+kty = 1;                      % Y axis thetadot factor
+ktz = 1;                      % Z axis thetadot factor
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Simulation Initializations
@@ -40,15 +41,15 @@ I = diag([5e-3, 5e-3, 10e-3]);  % Inertia Matrix
 
 % Simulation start and end time 
 startTime = 0;                  % Start time of the simulation (s)
-endTime = 20;                   % End time of the simulationh (s)
-dt = 0.0005;                     % Steps
+endTime = 40;                   % End time of the simulationh (s)
+dt = 0.005;                     % Steps
 
 times = startTime:dt:endTime;   % Vector with all the times
 N = numel(times);               % #of times that simulation will run
 
 % 4 DOF, X Y Z coordinate and direction
 startPoint = [0, 0, 0];         % start point
-endPoint = [2, 0, 10];          % end point
+endPoint = [0, 3, 10];          % end point
 
 theta_x_des = 0;                % x end direction
 theta_y_des = 0;                % y end direction
@@ -60,7 +61,7 @@ vel_x_des = 0;
 % Output values, recorded as the simulation runs
 weight = [0;0;-m*g;];
 
-
+error = [];
 x_out = zeros(3,N);                 % mass position [X-Y-Z]
 v_out = zeros(3,N);                 % mass velocity [X-Y-Z]
 a_out = zeros(3,N);                 % mass acceleration [X-Y-Z]
@@ -70,9 +71,10 @@ omega_out = zeros(3, N);            % Angular velocities on yaw, pitch and roll
 omegadot_out = zeros(3, N);         % Angular acceleration on yaw, pitch and roll
 thrust_out = zeros(3, N);           % vector with values only on z axis
 torque_out = zeros (3, N);          % Torque of yaw, pitch, roll
+engine_RPM_out = zeros(4, N);
+F_des_out = zeros(3, N);
 
-
-F_des = 0;
+T = [0.01 0.01 0.01]';
 vector = [0 0 0 0]';
 engine_RPM = [0 0 0 0]';
 torque = [0 0 0]';
@@ -84,32 +86,41 @@ omega = [0 0 0]';
 omegadot = [0 0 0]';
 theta = [0 0 0]';
 thetadot = [0 0 0]';            
-
+gg = zeros(1, N);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Simulation LOOP
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%index = 1;
+error2 = inf;
+error_sum = 0;
+%while error2>1 && index<4000
 for index = 1:1:N
     % Desired force controller
-    F_des = (m*g)+kfx*(endPoint(3)-x(3))-kfv*v(3);
+    e = endPoint' - x;
+    edot = 0 - v;
+%     for i = 1:index
+%         error_sum = error_sum + e;
+%     end
+    T = [0.001 0.0001 m*g]' + kp*e + ki*error_sum + kd*edot;
 
     % Torque controller
     vel_x_des = kvx*(endPoint(1) - x(1));
-    theta_x_des = (m/F_des)*kthetax*(vel_x_des-v(1));
-    
+    theta_x_des = (m/T(3))*kthetax*(vel_x_des - v(1));
+   
     vel_y_des = kvy*(endPoint(2) - x(2));
-    theta_y_des = (m/F_des)*kthetay*(vel_y_des-v(2));
+    theta_y_des = (m/T(3))*kthetay*(vel_y_des - v(2))
     
   
-    torque(1) = kttx*(theta_x_des - theta(3)) - ktx*thetadot(3);
-    torque(2) = 0;%ktty*(theta_y_des - theta(2)) - kty*thetadot(2);
+    torque(1) = 0;%kttx*(theta_x_des - theta(3)) - ktx*thetadot(3);
+    torque(2) = ktty*(theta_y_des - theta(2)) - kty*thetadot(2);
     torque(3) = kttz*(theta_z_des - theta(1)) - ktz*thetadot(1);
 
     % Calculate the thrust according to the above desired force and torque
-    vector = [F_des, torque(1), torque(2), torque(3)]';
-    gamma = [ct, ct, ct, ct; 0, d*ct, 0, -d*ct; -d*ct, 0, d*ct, 0; -cq, cq, -cq, cq];
-    engine_RPM = sqrt(abs(gamma\vector)); % kw
+    vector = [T(3), torque(1), torque(2), torque(3)]';
+    gamma1 = [ct, ct, ct, ct; 0, d*ct, 0, -d*ct; -d*ct, 0, d*ct, 0; -cq, cq, -cq, cq];
+    engine_RPM = gamma1\vector; % kw
     R = rotation(theta);
-    thrust = [0; 0; ct * sum(engine_RPM.^2)];
+    thrust = [0; 0; ct * sum(abs(engine_RPM))];
 
     a = (weight + R*thrust)/m;
     v = v + dt * a;
@@ -118,12 +129,12 @@ for index = 1:1:N
     omegadot = I\(cross(-omega, I*omega) + torque);
     omega = omega + dt * omegadot;
     
-    psi = theta(1);
-    phi = theta(2);
+    psi1 = theta(1);
+    phi1 = theta(2);
     W = [
-        0, cos(psi), -cos(phi)*sin(psi)
-        0, sin(psi), cos(phi)*cos(psi)
-        1, 0, sin(phi) 
+        0, cos(psi1), -cos(phi1)*sin(psi1)
+        0, sin(psi1), cos(phi1)*cos(psi1)
+        1, 0, sin(phi1) 
     ];
  
     thetadot = W\omega;
@@ -138,10 +149,17 @@ for index = 1:1:N
     omega_out(:, index) = omega;
     thetadot_out(:, index) = thetadot;
     theta_out(:, index) = theta;
+    error(index) = norm(endPoint'-x);
+    error2 = error(index);
+    %index = index+1;
+    engine_RPM_out(:, index) = engine_RPM;
+    F_des_out(:, index) = T;
 end
+
+%plot(F_des_out(end,:), 'Linewidth', 3)
 
 % Animation
 figure('units','normalized','outerposition',[0 0 1 1], 'KeyPressFcn', @visualize);
-data = struct('x', x_out, 'v', v_out, 'a', a_out, 'torque', torque, 'theta', theta_out,...
-    'times', times, 'dt', dt, 'eng_RPM', engine_RPM, 'F_des', F_des);
+data = struct('x', x_out, 'v', v_out, 'a', a_out, 'torque', torque_out, 'theta', theta_out,...
+    'times', times, 'dt', dt, 'eng_RPM', engine_RPM_out, 'F_des', T);
 visualize(data);
