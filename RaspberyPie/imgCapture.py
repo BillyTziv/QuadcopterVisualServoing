64d3a24acaafbdbe6e21c823e1c25dfb2af72785
mbalamat <mbalamatsias@gmail.com>
2015-12-23 17:39:48 +0200

    Add image capture

--- RaspberyPie/imgCapture.py
+++ RaspberyPie/imgCapture.py
@@ -1,5 +1,6 @@
 import time
 import RPi.GPIO as GPIO
+import os
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.IN)
@@ -12,6 +13,7 @@ try:
 		if (GPIO.input(11) == 1):
 			print "ON";
 			time.sleep(50.0/1000.0);
+            os.system("./rimg.sh")
 		else:
 			print "OFF";
 			time.sleep(50.0/1000.0);
