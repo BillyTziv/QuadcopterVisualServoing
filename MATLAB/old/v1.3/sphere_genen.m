aab31169f3d32bfe9475d628bf7b42539ff11f8c
Vasilis Tzivaras <vtzivaras@gmail.com>
2016-11-21 00:21:13 +0200

    deleted files

--- MATLAB/old/v1.3/sphere_genen.m
+++
@@ -1,4 +0,0 @@
-[X,Y,Z] = sphere(50);
-
-figure; mesh(X,Y,Z);
-axis([-10 10 -10 10 -10 10]);
--- RaspberyPie/rimg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-TE=$(date +"%Y-%m-%d_%H%M")
-
-raspistill -vf -hf -o /home/pi/camera/$DATE.jpg
